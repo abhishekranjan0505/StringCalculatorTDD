79daed6c819f8a994d697d627b21acc01a9a5835
abhishekranjan0505 <abhishek.ranjan0505@gmail.com>
2024-06-08 20:52:06 +0530

    project setup

---
+++ src/StringCalculator.java
@@ -0,0 +1,14 @@
+public class StringCalculator {
+    public int add(String s){
+        return 0;
+    }
+    public int subtract(String s){
+        return 0;
+    }
+    public int multiply(String s){
+        return 0;
+    }
+    public int divide(String s){
+        return 0;
+    }
+}
---
+++ src/StringCalculatorTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    public void add() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void subtract() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void multiply() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void divide() throws Exception {
+        fail("This test has yet to be implemented");
+    }
+}
